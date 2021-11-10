b39e7da37e76776c29de8f3b544bf8741489d635
Carlos Vasquez <cevasquezf22@gmail.com>
2021-11-09 19:33:00 -0500

    Utilizando try, except and raise

--- debugging.py
+++ debugging.py
@@ -1,15 +1,25 @@
 def divisors(num):
-    divisors =[]
-    for i in range(1, num+1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
-    
+    try:
+        divisors =[]
+        if num < 0:
+            raise ValueError("debes ingresar un numero positivo")
+        else:
+            for i in range(1, num+1):
+                if num % i == 0:
+                    divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
+
 
 def run():
-    num = int(input("ingresa un numero: "))
-    print(divisors(num))
-    print("termino el programa")
+    try:
+        num = int(input("ingresa un numero: "))
+        print(divisors(num))
+        print("termino el programa")
+    except ValueError:
+        print("Debes ingresar un numero")
 
 if __name__ == '__main__':
     run()
