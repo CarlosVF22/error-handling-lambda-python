91c0e02751b0793cca76d6c96b0d44daf616af2d
Carlos Vasquez <cevasquezf22@gmail.com>
2021-11-13 16:30:32 -0500

    Juego terminado

--- juego_ahorcado.py
+++ juego_ahorcado.py
@@ -24,11 +24,14 @@
 # imprimir el arr_game en consola
 # Limpiar pantalla de los arr_game viejos
 # arreglar los caracteres de la data - todo
+# agregar alerta de ganador
+# agregar vidas y reducirlas al equivocarse
+# agregar advertencia al perder7
+# agregar dibujos con ascii
 
 import random
 import os
 import time
-
 
 def read(data):
     words = []
@@ -39,44 +42,86 @@ def read(data):
     return words
 
 
-def iterate(string, search_word, data_game, arr_game):
+def iterate(string, search_word, data_game, arr_game, life_game):    
     arr = [word for word in string]
+    n_arr = len(arr)
+    dic_game = {
+        "arr_game" : arr_game,
+        "life_game" : life_game
+    }
+    arr_game_initial = dic_game.get("arr_game")
+    life_game_initial = dic_game.get("life_game")
+
+    arr_game = arr_game_initial.copy()
+
     
     position = 0
     for word in arr:
-        if word == search_word:
+        if word == search_word and position <= n_arr :
             arr_game.pop(position)
             arr_game.insert(position, word)
         position +=1
-    
+
     with open(data_game, "a", encoding='utf-8') as f:
         str_game = "".join(arr_game)
         f.write(str_game)
         f.write("\n")
 
-    return arr_game
+    if arr_game == arr:
+        arr_game = "Haz ganado"
+
+    if arr_game == arr_game_initial:
+        life_game -= 1
+    
+    if life_game == 0:
+        life_game = "Haz perdido"
+
+    actual_game = [arr_game, life_game]
+
+    return actual_game
 
 
 def run():
+    os.system("clear")
+    life_game = 5
     data_game = "./archivos/game.txt"
-    play = True
     data = './archivos/data.txt'
     words = read(data)
+    # words = words.encode(encoding = "utf-8", errors = "ignore")
     data_len = len(words)
     random_num = random.randint(0, data_len)
     word_game = "".join(words[random_num])
     arr_game = ["-" for item in word_game]
+
+    print("Bienvenido al juego del ahorcado")
+    print("Vidas: " + str(life_game))
     print(arr_game)
-    print(words)
+    print(word_game)
 
 
-    while play:
+    while life_game > 0:
+
         word_input = input("Ingresa una letra: ")
         time.sleep(0.5)
         os.system("clear")
-        arr_game= iterate(word_game,word_input, data_game, arr_game)
+
+        actual_game= iterate(word_game,word_input, data_game, arr_game, life_game)
+        arr_game = actual_game[0]
+        life_game = actual_game[1]
+        if arr_game == "Haz ganado":
+            print(arr_game)
+            break
+        if life_game == "Haz perdido":
+            print(life_game)
+            break
+
+        print("Bienvenido al juego del ahorcado")
+        print("Vidas: " + str(life_game))
         print(arr_game)
         print(word_game)
+
+        
+
 
 
 if __name__ == '__main__':
